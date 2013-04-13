858ce8115c9e81979948fb2593d4c109a0fc9841
Luke Francl <lfrancl@swiftype.com>
2013-04-12 18:47:10 -0700

    Update jquery.ba-hashchange  and search library to work in jQuery 1.9

--- jquery.swiftype.search.js
+++ jquery.swiftype.search.js
@@ -143,7 +143,7 @@
         });
       }
 
-      $('[data-hash][data-page]').live('click', function (e) {
+      $(document).on('click', '[data-hash][data-page]', function (e) {
         e.preventDefault();
         var $this = $(this);
         setSearchHash($.hashParams().stq, $this.data('page'));
